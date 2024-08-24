61fb84547872a18367c34d1372fdda657b1737b3
ibrahem <habboushebraheem@gmail.com>
2024-08-24 19:04:10 +0300

    update2

--- src/loop/loop.java
+++ src/loop/loop.java
@@ -3,8 +3,42 @@ package loop;
 public class loop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hallo ");
+//this is the list of all nationalites valid and not valid
+		String[] nationality = { "jordan", "syria", "jordan", "oman", "egypt", "hello", "bye", "eraq" };
+		// those are the name of the people in coptition
+		String[] names = { "rowan", "layan", "ahmad", "ali", "anas", "omar", "haneen", "haya" };
+		// the age of people
+		int[] age = { 17, 18, 79, 80, 41, 8, 18, 34 };
+		// this is the fillter for valid nationalites
+		String[] validnationality = { "jordan", "syria", "oman", "egypt", "eraq" };
+		for (int i = 0; i < names.length; i++) {
+			boolean isvalidnationality = false;
+			boolean isvalidage = age[i] > 17 && age[i] < 100;
+			for (String validnationalityy : validnationality) {
 
+				if (nationality[i].equalsIgnoreCase(validnationalityy)) {
+					isvalidnationality = true;
+					break;
+				}
+
+			}
+			if (isvalidnationality && isvalidage) {
+				if (nationality[i].equals("jordan") && age[i] >= 18) {
+					// the people are jordanian and ages=>18.
+					System.out.println("hay " + names[i] + " you have been selected");
+				} else {
+					// the people are not jordanian but the age >=18.
+					System.out.println("sorry " + names[i] + " you have not been selected becose the nationality is "
+							+ nationality[i] + " while is age is ok");
+				}
+			} else if (isvalidage == false && isvalidnationality) {
+				// the people are jordanian but the age<18.
+				System.out.println("sorry " + names[i] + " you have not been selected becouse the age is ander 18");
+
+			}
+
+		}
 	}
 
 }
